24dfee3971e6765ae32bee4e1037bad447ddddbb
dristiron03 <41644787+dristiron03@users.noreply.github.com>
2019-08-25 11:11:13 +0530

    Update up.cpp

--- up.cpp
+++ up.cpp
@@ -1,185 +1,203 @@
-#include <bits/stdc++.h>
-#define lli long long int
-#define pb push_back
-#define vt vector
-#define f(x, y, z) for (lli x = y; x < z; x++)
+#include<bits/stdc++.h>
 using namespace std;
-vector<string> stringTovector(string s)
 
+int isoperator(string c)
+{
+    if (c == "^" || c == "/" || c == "*" || c == "+" || c == "-"||c=="(")
+        return 1;
+    return 0;
+}
+int inpre(string c)
+{
+    if (c == "^")
+        return 5;
+    if (c == "*" || c == "/")
+        return 4;
+    if (c == "+" || c == "-")
+        return 2;
+    else 
+        return 1;
+}
+
+int outpre(string c)
+{
+    if (c == "^")
+        return 6;
+    if (c == "*" || c == "/")
+        return 3;
+    if (c == "+" || c == "-")
+        return 1;
+    else
+        return -1;
+    
+}
+vector<string> stov(string s)
 {
     int n = s.size();
-    vector<string> st;
-    for(int i = 0; i<n;i++)
-        { string temp="";
-        if((s[i] >= '0' && s[i] <= '9')) 
-        { int j = i;
-          for(;(s[j] >= '0' && s[j] <= '9');j++){
-                temp+=s[j];}
-            i = j-1;
+    vector<string> v;
+    for (int i = 0; i < n; i++)
+    {
+        string temp = "";
+        if ((s[i] >= '0' && s[i] <= '9'))
+        {
+            int j = i;
+            while (i != s.size() && s[i] >= '0' && s[i] <= '9')
+            {
+                temp += s[i];
+                i++;
+            }
+            i--;
         }
-        else 
-        {temp+=s[i];
-          }st.push_back(temp);}
-        return st;
+        else
+        {
+            temp += s[i];
+        }
+        if (temp=="-")
+        {
+            if (v.empty())
+                v.push_back("0");
+            else
+            {
+                if (isoperator(v.back()))
+                    v.push_back("0");
+            }
+        }
+        v.push_back(temp);
+    }
+    return v;
 }
-int inprec(char input) 
-{ 
+vector<string> post(vector<string> s)
+{
+    stack<string> st;
+    st.push("E");
+    int l = s.size();
+    vector<string> nv;
+    for (int i = 0; i < l; i++)
+    {
+        if ((s[i][0] >= '0' && s[i][0] <= '9'))
+            nv.push_back(s[i]);
+        else if (s[i] == "(")
+            st.push("(");
+        else if (s[i] == ")")
+        {
+            while (st.top() != "E" && st.top() != "(")
+            {
+                string c = st.top();
+                st.pop();
+                nv.push_back(c);
+            }
+            if (st.top() == "(")
+            {
+                string c = st.top();
+                st.pop();
+            }
+        }
+        else
+        {
+            while (st.top() != "E" && outpre(s[i]) < inpre(st.top()))
+            {
+                string c = st.top();
+                st.pop();
+                nv.push_back(c);
+            }
+            st.push(s[i]);
+        }
+    }
+    while (st.top() != "E")
+    {
+        string c = st.top();
+        st.pop();
+        nv.push_back(c);
+    }
 
-    switch (input) { 
-    case '+': 
-    case '-': 
-      return 2; 
-    case '*': 
-    case '%':  
-    case '/':  
-      return 4;
-    case '^':  
-        return 5; 
-    case '(': 
-        return 0; 
-    } 
-} 
-
-int outprec(char input) 
-{ 
-    switch (input) 
-    { 
-    case '+': 
-    case '-': 
-        return 1; 
-    case '*': 
-    case '%': 
-    case '/': 
-        return 3; 
-    case '^': 
-        return 6; 
-    case '(': 
-        return 100; 
-    } 
-} 
-
-vector<string> infixToPostfix(vector<string> s) 
-{ 
-    stack<string>st; 
-    st.push("N"); 
-    int l = s.size(); 
-    vector<string> ns;
-    for(int i = 0; i < l; i++) 
-    { if((s[i][0] >= '0' && s[i][0] <= '9')) 
-            {ns.push_back(s[i]);}
-        else if(s[i] == "(") 
-           {st.push("("); }
-        else if(s[i] == ")") 
-        { 
-          while(st.top() != "N" && st.top() != "(") 
-            { string c = st.top(); 
-                st.pop(); 
-                ns.push_back(c);
-              } 
-            if(st.top() == "(") 
-            { 
-                string c = st.top(); 
-                st.pop(); } } 
-      else{ 
-          while(st.top() != "N" && outprec(s[i][0]) < inprec((st.top())[0])) { 
-                string c = st.top(); 
-                st.pop(); 
-                ns.push_back(c); 
-            } 
-            st.push(s[i]); 
-        } 
-
-  
-
-    } 
-
-    while(st.top() != "N") 
-    { 
-        string c = st.top(); 
-        st.pop(); 
-        ns.push_back(c); 
-    } 
-    return ns;
-} 
-
+    return nv;
+}
 struct node
 {
-  string s;
-  node* lch=NULL,*rch=NULL;
+    string s;
+    node *lch = NULL, *rch = NULL;
 };
-
-node* construct_tree(vt <string> postfix)
+node *ctree(vector<string> str)
 {
-   node *root=NULL;
-   vt <node*> stk;
-   int i=0;
-   while(i!=postfix.size())
-   {
-     if(postfix[i]!="+" && postfix[i]!="-" && postfix[i]!="*" && postfix[i]!="/" && postfix[i]!="^")
-       {
-         node *temp=(node *)malloc(sizeof(node));
-         temp->s=postfix[i];
-         f(j,0,postfix[i].size())
-         if(postfix[i][j]<'0' || postfix[i][j]>'9')return NULL;
-         stk.pb(temp);
-       } 
-     else
-     {
-       if(stk.size()<2)return NULL;
-       node *s1=stk.back();
-       stk.pop_back();
-       node *s2=stk.back();
-       stk.pop_back();
-       node *temp=(node *)malloc(sizeof(node));
-       temp->s=postfix[i];
-       temp->lch=s2;
-       temp->rch=s1;
-       stk.pb(temp); 
-     }
-     i++;
-   }
-   if(stk.size()!=1)return NULL;
-   return stk.back();
-}
 
-lli evaluate(node *root)
-{
-   string s=root->s;
-
-   lli ans=0;
-
-   if(s!="+" && s!="-" && s!="*" && s!="/" && s!="^")
-   {return stoi(s);}
-  else{
-      int lc=evaluate(root->lch),rc=evaluate(root->rch);
-      if(s=="+"){ans=lc+rc;}
-      else if(s=="-"){ans=lc-rc;}
-      else if(s=="*"){ans=lc*rc;}
-      else if(s=="/"){ans=lc/rc;}
-      else{ans=1;
-        f(i,0,rc){ans*=lc;}
-      }
-  }
-  return ans;
-}
-int main() 
-{ 
-  int t;
-  cin>>t;
-  while(t--)
-  {   string s;
-      cin>>s;
-      vector<string> postfix=stringTovector(s);
-      postfix=infixToPostfix(postfix);
-      node* root=construct_tree(postfix);
-      if(root!=NULL)
-      {
-      cout<<evaluate(root)<<"\n";
-      }
-      else{cout<<"CAN'T BE EVALUATED\n";}
+    node *root = NULL;
+    vector<node *> stv;
+    int i = 0;
+    while (i != str.size())
+    {
+        if (str[i] != "+" && str[i] != "-" && str[i] != "*" && str[i] != "/" && str[i] != "^")
+        {
+            node *temp = (node *)malloc(sizeof(node));
+            temp->s = str[i];
+            for(int j=0;j<str[i].size();j++)
+            {
+                if (str[i][j] < '0' || str[i][j] > '9')
+                    return NULL;
+            }
+            stv.push_back(temp);
+        }
+        else
+        {
+            if (stv.size()< 2)
+                return NULL;
+            node *s1 = stv.back();
+            stv.pop_back();
+            node *s2 = stv.back();
+            stv.pop_back();
+            node *temp = (node *)malloc(sizeof(node));
+            temp->s = str[i];
+            temp->lch = s2;
+            temp->rch = s1;
+            stv.push_back(temp);
+        }
+        i++;
     }
-  return 0;
+    if (stv.size()!= 1)
+        return NULL;
+    return stv.back();
 }
-
-
-
+int value(node *root)
+{
+    string s = root->s;
+    int ans = 0;
+    if (s != "+" && s != "-" && s != "*" && s != "/" && s != "^")
+        return stoi(s);
+    else
+    {
+        int lans = value(root->lch), rans = value(root->rch);
+        if (s == "+")
+            ans = lans + rans;
+        else if (s == "-")
+            ans = lans - rans;
+        else if (s == "*")
+            ans = lans * rans;
+        else if (s == "/")
+            ans = lans / rans;
+        else
+        {
+            ans = 1;
+            for(int i=0;i<rans;i++) ans *= lans;
+        }
+    }
+    return ans;
+}
+int main(){
+    int t, n;
+    cin >> t;
+    while (t--){
+        cin >> n;
+        while (n--)
+        {
+            string s;
+            cin >> s;
+            vector<string> str = stov(s);
+            str = post(str);
+            node *root = ctree(str);
+            if (root == NULL)
+                cout << "CANT BE EVALUATED" << endl;
+            else
+                cout << value(root) << endl;
+        }
+    }
+    return 0;
+}
